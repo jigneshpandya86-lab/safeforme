d592358bc94a44202776e306a5981b80878eccc1
jigneshpandya86-lab <jigneshpandya86@gmail.com>
2026-05-17 14:19:01 +0530

    Fix router basename for GitHub Pages path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/report/:locationId" element={<SafetyReport />} />
